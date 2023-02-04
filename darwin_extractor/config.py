2e8db6e21993f1859c00e5991ae67e5a965d7dd2
UsmanKhanAbasi <usman.khan.abasi@wolt.com>
2023-02-04 16:35:02 +0100

    Add comment

--- darwin_extractor/config.py
+++ darwin_extractor/config.py
@@ -5,4 +5,4 @@ from pydantic.fields import Field
 class DarwinConfig(BaseSettings):
     # TODO: The token will come from environment variable, never ever store secret and tokens in the code. This is
     #  for the sake of this task only. In production code, secrets should alwasy come from environment variables.
-    access_token: str = Field(default="c570fb40-f784-3dd8-b784-ab29c2115986", env="ACCESS_TOKEN")
+    access_token: str = Field(default="add_your_token_here", env="ACCESS_TOKEN")
